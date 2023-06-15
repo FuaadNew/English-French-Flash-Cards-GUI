ded00b052794f6dfe08b217ca0d098c11de795f4
FuaadNew <134631945+FuaadNew@users.noreply.github.com>
2023-06-15 18:52:03 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,63 @@
+BACKGROUND_COLOR = "#B1DDC6"
+from tkinter import *
+import pandas
+import random
+timer = None
+current_card = {}
+to_learn = {}
+
+
+
+
+def next_card():
+        global current_card,flip_timer
+        window.after_cancel(flip_timer)
+        current_card = random.choice(to_learn)
+        canvas.itemconfig(card_title, text= "French",fill = "black")
+        canvas.itemconfig(card_word,text=current_card["French"],fill="black")
+        canvas.itemconfig(card_background,image=card_front_img)
+        flip_timer= window.after(3000, func=flip_card)
+def flip_card():
+        canvas.itemconfig(card_title, text= "English",fill = "white")
+        canvas.itemconfig(card_word,text= current_card["English"],fill = "white")
+        canvas.itemconfig(card_background,image=card_back_img)
+
+def is_known():
+        to_learn.remove(current_card)
+        data = pandas.DataFrame(to_learn)
+        data.to_csv("data/words_to_learn.csv",index = False)
+        next_card()
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50,bg=BACKGROUND_COLOR)
+flip_timer= window.after(3000,func=flip_card)
+try:
+        data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+        original_data = pandas.read_csv("data/french_words.csv")
+        to_learn = original_data.to_dict(orient = "records")
+else:
+        to_learn = data.to_dict(orient= "records")
+
+to_learn= data.to_dict(orient = "records")
+
+
+canvas = Canvas(height=526, width=800)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file= "images/card_back.png")
+card_background= canvas.create_image(400,263, image=card_front_img)
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0, column=0,columnspan=2)
+card_title= canvas.create_text(400,158,text="Title",font= ("Ariel",40,"italic"))
+card_word= canvas.create_text(400,263,text ="Word",font= ("Ariel",60,"bold"))
+check = PhotoImage(file="images/right.png")
+wrong= PhotoImage(file="images/wrong.png")
+X_button = Button(image=check,highlightthickness=0,bg=BACKGROUND_COLOR,command=next_card)
+X_button.grid(row=1, column=0)
+CHECK_button = Button(image=wrong,highlightthickness=0,bg=BACKGROUND_COLOR,command= is_known)
+CHECK_button.grid(row=1, column=1)
+next_card()
+
+
+window.mainloop()
